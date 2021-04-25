e70fc97ef97899c187775340ddc7e59212d5949b
sapschernig <sapschernig@edu.aau.at>
2021-04-25 23:28:25 +0200

    final commit

--- src/main/java/ab1/impl/LaggerSchneiderPschernig/Ab1Impl.java
+++ src/main/java/ab1/impl/LaggerSchneiderPschernig/Ab1Impl.java
@@ -2,38 +2,114 @@ package ab1.impl.LaggerSchneiderPschernig;
 
 import ab1.Ab1;
 
+
+
 public class Ab1Impl implements Ab1 {
+
+
 
 	@Override
 	public boolean isHeap(int i, int j, int[] data) {
-		// YOUR CODE HERE
-		return false;
+
+		/* algorithm to check if array fulfills heap-condition
+		i= beginning of array
+		j= end of array
+		 */
+
+		//an array with a length of one qualifies as heap, return true
+		if (data.length==1){
+			return true;
+		}
+
+		else if (i*2+1>j && j/2<j*2+1){
+			return false;
+		}
+		//checks if conditions are not met to be heap, returns false
+		for (int k = i; k < j/2; k++) {
+			if (data[k-1]<data[2*k-1] || data[k-1]<data[2*k]){
+				return false;
+			}
+		}
+		//otherwise return true
+		return true;
+		}
+
+	@Override
+	public void toHeap(int[] data) { //Algorithmus zum Aufbau eine Max-Heaps
+		for (int i = data.length / 2; i > 0; i--) {
+			buildHeap(data, i, data.length);
+		}
+	}
+
+	//method to build Heap, as seen on the lecture notes
+	private void buildHeap(int[]data, int i, int j){
+		int k=i;
+		int m=2*k;
+		int x=data[i-1];
+
+		while (m<=j){
+			if (m<j && data[m-1]<data[m]){
+				m=m+1;
+			}
+			if (x>=data[m-1]){
+				break;
+			}
+			data[k-1]=data[m-1];
+			k=m;
+			m=2*k;
+		}
+		data[k-1]=x;
+
+	}
+	//swap method
+	private void swap(int[]data, int i, int childIndex){
+		int k=data[i];
+		data[i]=data[childIndex];
+		data[childIndex]=k;
 	}
 
 	@Override
-	public void toHeap(int[] data) {
-		// YOUR CODE HERE
-	}
+	public void heapsort(int[] data) { //Heapsort-Algorithm
+		//always be aware that array index starts with 0 but heap with 1
+		for (int i = (data.length / 2) ; i > 0; i--) {
+			buildHeap(data, i, data.length);
+		}
 
-	@Override
-	public void heapsort(int[] data) {
-		// YOUR CODE HERE
-	}
+			//sorting
+			for (int i = data.length-1; i > 0; i--) {
+				swap(data, i, 0);
+				buildHeap(data, 1, i);
+				//isHeap(0,i,data);
+			}
+		}
+
 
 	@Override
 	public ListNode insert(ListNode head, int value) {
 
-
-
-
-		return head;
+		return null;
 	}
+
 
 	@Override
 	public ListNode search(ListNode head, int value) {
+		/*
+		head - list to search through
+		value - to value to search for
+		return - first element with searched value
+		 */
 
 
-
+		//check as long as the pointer doesn't point to null, which means the end of the list
+		while (head!=null) {
+			//check if the value in the current element equals the value that is being searched for
+			if (head.value == value) {
+				return head;
+				//if not, then move to next element
+			} else {
+				head=head.next;
+			}
+		}
 		return null;
 	}
 
